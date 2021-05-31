9ecf8f3180747e0db44c13dfbbd3370f9d2720fc
Nadine Mili <mili.nadine07@gmail.com>
2021-05-31 22:28:49 +0100

    affichage le nom de l'utilisateur dans le sidebar

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -16,16 +16,27 @@ import {Role} from '../../pages/models/role';
 export class SidebarComponent implements OnInit, OnDestroy {
     info: any;
     activeMenuItem$: Observable<Menu>;
+    roles: any;
+    username: string;
     authListenerSubs: Subscription = new Subscription();
 
-    constructor(private token: TokenStorageService,
-                private clientService: ClientsService,
-                public authService: AuthService) {}
+    constructor(private tokenStorage: TokenStorageService,
+                public authService: AuthService) {
+    }
 
     ngOnInit() {
+        this.getRole();
+        this.getUsername();
     }
+
     ngOnDestroy() {
         this.authListenerSubs.unsubscribe();
     }
 
+    getRole() {
+        this.roles = this.tokenStorage.getAuthorities();
+    }
+    getUsername(){
+        this.username = this.tokenStorage.getUsername();
+    }
 }
